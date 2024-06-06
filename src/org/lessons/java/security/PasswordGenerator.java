172789afa5d1dbf663133bfd031781e050960f0b
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-06 15:45:00 +0200

    password generated

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,10 +1,37 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner inputUser = new Scanner(System.in);
+		
+		System.out.println("Inserisci il tuo nome");
+		String userName = inputUser.nextLine();
+		
+		System.out.println("Inserisci il tuo cognome");
+		String userSurname = inputUser.nextLine();
+		
+		System.out.println("Inserisci il tuo colore preferito");
+		String userColor = inputUser.nextLine();
+		
+		System.out.println("Insersci il giorno in cui sei nato");
+		int userDay = inputUser.nextInt();
+		
+		System.out.println("Insersci il mese in cui sei nato");
+		int userMonth = inputUser.nextInt();
+		
+		System.out.println("Insersci l'anno in cui sei nato");
+		int userYear = inputUser.nextInt();
+		
+		int userAge = (userDay + userMonth + userYear);
+		
+		String password = (userName+"-"+userSurname+"-"+userColor+"-"+userAge);
+		
+		System.out.println("La password segreta generata è: " + password);
+		
 	}
 
 }
--- src/org/lessons/java/security/package-info.java
+++
@@ -1,1 +0,0 @@
-package org.lessons.java.security;
